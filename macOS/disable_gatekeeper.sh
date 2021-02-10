275187cdafd56f742ec0734a71abf34a0a1a3049
JPMike <jpmikemessage@gmail.com>
2021-02-10 17:22:49 +0800

    change to english

--- macOS/disable_gatekeeper.sh
+++ macOS/disable_gatekeeper.sh
@@ -1,3 +1,3 @@
-#  Mac上安全地打开应用是按住Control键点按应用
-# 全局设置需要禁用Gatekeeper，禁用后在设置的安全性与隐私里能看到选项有变化
+# Press Control Button to open app safely on macOS
+# to change this setting globally, disable Gatekeeper. Then check safety and privacy in setting.
 sudo spctl --master-disable
